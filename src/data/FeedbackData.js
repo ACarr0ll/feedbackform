ace69a8d741ae5298e0666cef087aacb4e9ba85a
ACarr0ll <carroll.andrew@gmail.com>
2022-05-19 21:37:15 -0400

    useState and Props practice

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo distinctio odio qui libero beatae aliquam quaerat voluptas magni quia. Deserunt ut natus ipsum esse quasi?'
+    },
+    {
+        id: 2,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo distinctio odio qui libero beatae aliquam quaerat voluptas magni quia. Deserunt ut natus ipsum esse quasi?'
+    },
+    {
+        id: 3,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo distinctio odio qui libero beatae aliquam quaerat voluptas magni quia. Deserunt ut natus ipsum esse quasi?'
+    }
+]
+
+export default FeedbackData
