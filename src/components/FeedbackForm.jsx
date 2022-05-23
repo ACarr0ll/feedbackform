1d2f03d71b93d6d4cb9ebee2ea2db3451809860e
ACarr0ll <carroll.andrew@gmail.com>
2022-05-23 19:20:45 -0400

    Form validation or 10 characters

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,21 @@ import { useState } from 'react'
 
 function FeedbackForm() {
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     const handleTextChange = (e) => {
+        if(text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
         setText(e.target.value)
     }
 
@@ -16,8 +29,10 @@ function FeedbackForm() {
             <h2>How would you rate your service with us?</h2>
             <div className="input-group">
                 <input type="text" placeholder='Write a review' onChange={handleTextChange} value={text} />
-                <Button type='submit'> Send </Button> 
+                <Button type='submit'isDisabled={btnDisabled}> Send </Button> 
             </div>
+
+            {message && <div className='message'>{message}</div>}
         </form>
     </Card>
   )
