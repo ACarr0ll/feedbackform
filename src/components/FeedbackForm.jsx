0a374897be2dd6de169977bc57c040303dac2343
ACarr0ll <carroll.andrew@gmail.com>
2022-05-23 20:11:38 -0400

    Add animation/motion

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,7 @@ import Button from './shared/Button'
 import RatingSelect from './RatingSelect'
 import { useState } from 'react'
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
     const [text, setText] = useState('')
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
@@ -25,9 +25,23 @@ function FeedbackForm() {
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10 ){
+            const newFeedback = {
+                text,
+                rating
+            }
+
+            handleAdd(newFeedback)
+
+            setText('')
+        }
+    }
+
   return (
     <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service with us?</h2>
             <RatingSelect  select={(rating) => setRating(rating)}/>
             <div className="input-group">
